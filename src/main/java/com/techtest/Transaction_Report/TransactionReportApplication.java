93cb8451e9c2e32f2f074890aa34d0ccda817b02
SnehalGawde <42560623+SnehalGawde@users.noreply.github.com>
2018-08-22 09:45:00 +1000

    Update TransactionReportApplication.java

--- src/main/java/com/techtest/Transaction_Report/TransactionReportApplication.java
+++ src/main/java/com/techtest/Transaction_Report/TransactionReportApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
 
-import java.io.IOException;
 import java.nio.file.*;
 import java.util.Map;
 import java.util.stream.Collectors;
