2a7b71ffd7557706aaccd46c042b86ed865b4ae6
SnehalGawde <snehal9969@gmail.com>
2018-08-22 09:16:38 +1000

    First commit

---
+++ src/main/java/com/techtest/Transaction_Report/TransactionReportApplication.java
@@ -0,0 +1,67 @@
+package com.techtest.Transaction_Report;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.CommandLineRunner;
+
+import java.io.IOException;
+import java.nio.file.*;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class TransactionReportApplication implements CommandLineRunner{
+
+    @Value("${InputFile}")
+    String strInputFile;
+
+    @Value("${OutputFile}")
+    String strOutputFile;
+
+    Path inputFilepath, outputFilepath;
+
+    private static final Logger logger = LoggerFactory.getLogger(TransactionReportApplication.class);
+
+	public static void main(String[] args) {SpringApplication.run(TransactionReportApplication.class, args);}
+
+	@Override
+	public void run(String... args){
+	    try {
+            inputFilepath = Paths.get(strInputFile);
+            outputFilepath = Paths.get(strOutputFile);
+
+            Map<String, Map<String, Long>> mpReport = readInputFile(inputFilepath);
+            writeOutputFile(mpReport, outputFilepath);
+
+        }
+        catch (Exception ex){
+            logger.error(ex.getMessage(), ex);
+        }
+	}
+
+	public Map<String, Map<String, Long>> readInputFile(Path inputFilepath) throws Exception {
+	    return Files.lines(inputFilepath)
+                .collect(Collectors.groupingBy(line -> line.substring(3, 19),
+                        Collectors.groupingBy(line -> line.substring(25, 45),
+                                Collectors.summingLong(
+                                        line -> Integer.parseInt(line.substring(51, 62).trim()) - Integer.parseInt(line.substring(62, 73).trim())
+                                )
+                        )
+                ));
+    }
+
+    public void writeOutputFile(Map<String, Map<String, Long>> mpReport, Path outputFilepath) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Client_Information,Product_Information,Total_Transaction_Amount\n");
+        for (String keyClient : mpReport.keySet()) {
+            for (String keyProduct: mpReport.get(keyClient).keySet()) {
+                sb.append(keyClient+","+keyProduct+","+mpReport.get(keyClient).get(keyProduct)+"\n");
+            }
+        }
+
+        Files.write(outputFilepath, sb.toString().getBytes());
+    }
+}
---
+++ src/test/java/com/techtest/Transaction_Report/TransactionReportApplicationTests.java
@@ -0,0 +1,96 @@
+package com.techtest.Transaction_Report;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.TestCase.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class TransactionReportApplicationTests {
+
+	@InjectMocks
+	TransactionReportApplication app = new TransactionReportApplication();
+
+	Path inputFilepath;
+	Path outputFilepath = Paths.get("src/test/resources/Output.csv");
+
+	//		Client_Information 1, Product_Information 1, QUANTITY LONG > QUANTITY SHORT
+	@Test
+	public void testCase1() throws Exception {
+		inputFilepath = Paths.get("src/test/resources/InputTestCase1.txt");
+		assertEquals("{CL  432100020001={FUSGX NK    20100910=1}}", app.readInputFile(inputFilepath).toString());
+	}
+
+	//		Client_Information >1, Product_Information 1
+	@Test
+	public void testCase2() throws Exception{
+		inputFilepath = Paths.get("src/test/resources/InputTestCase2.txt");
+
+		Map<String, Map<String, Long>> mpActual = app.readInputFile(inputFilepath);
+
+		assertEquals(new Long(0), mpActual.get("CL  432122220001").get("FUSGX NK    20100910"));
+		assertEquals(new Long(1111111111), mpActual.get("CL  444400020001").get("FUSGX NK    20100910"));
+		assertEquals(new Long(0), mpActual.get("CL  432100021111").get("FUSGX NK    20100910"));
+		assertEquals(new Long(1111111111), mpActual.get("CL01432100020001").get("FUSGX NK    20100910"));
+	}
+
+	//		Client_Information 1, Product_Information >1
+	@Test
+	public void testCase3() throws Exception{
+		inputFilepath = Paths.get("src/test/resources/InputTestCase3.txt");
+
+		Map<String, Long> mpActualOutput = app.readInputFile(inputFilepath).get("CL  432100020001");
+
+		assertEquals(new Long(1234467891), mpActualOutput.get("FUSGX NK    20181230"));
+		assertEquals(new Long(0), mpActualOutput.get("FUSGX NK123A20100910"));
+		assertEquals(new Long(-1111111111L), mpActualOutput.get("FFSGX NK    20100910"));
+		assertEquals(new Long(0), mpActualOutput.get("FUSGX1NK    20100910"));
+	}
+
+	//		Client_Information >1, Product_Information >1
+	@Test
+	public void testCase4() throws Exception{
+		inputFilepath = Paths.get("src/test/resources/InputTestCase4.txt");
+
+		Map<String, Map<String, Long>> mpActualOutput = app.readInputFile(inputFilepath);
+
+		Map<String, Long> mpActualOutputCL1 = mpActualOutput.get("CL  444444420001");
+		Map<String, Long> mpActualOutputCL2 = mpActualOutput.get("CL  432122220001");
+
+		assertEquals(new Long(123456780), mpActualOutputCL1.get("FUSGX NK123A20100910"));
+		assertEquals(new Long(1357924671), mpActualOutputCL2.get("FUSGX1NK    20100910"));
+	}
+
+	@Test
+	public void testCase5() throws Exception{
+		inputFilepath = Paths.get("src/test/resources/InputTestCase4.txt");
+
+		Files.deleteIfExists(outputFilepath);
+		app.writeOutputFile(app.readInputFile(inputFilepath), outputFilepath);
+
+		assertTrue(Files.exists(outputFilepath));
+
+		List<String> lstActual = Files.readAllLines(outputFilepath);
+		List<String> lstExpected = new ArrayList<>();
+
+		lstExpected.add("Client_Information,Product_Information,Total_Transaction_Amount");
+		lstExpected.add("CL  444444420001,FUSGX NK123A20100910,123456780");
+		lstExpected.add("CL  432122220001,FUSGX1NK    20100910,1357924671");
+
+		assertEquals(lstActual,lstExpected);
+	}
+
+}
